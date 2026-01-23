caf89c607a39686c5a0bf39c7e3add8a1b2a659c
Amresh Kumar Gond <145150083+amresh91620@users.noreply.github.com>
2026-01-23 17:51:34 +0530

    Cleanup: removed duplicate files from root directory

--- middleware/adminMiddleware.js
+++
@@ -1,9 +0,0 @@
-const jwt  = require("jsonwebtoken");
-const { models } = require("mongoose");
-
-module.exports= (req,res,next)=>{
-    if(req.user || req.user.role !=="admin"){
-        return res.status(403).json({ msg: "Admin access only" });
-    }
-    next();
-};
